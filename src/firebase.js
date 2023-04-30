37e7479773639d56567114283ae0377d89facfa4
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-04-30 20:04:49 +0200

    add github pages2

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,15 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 // import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = process.env.FIREBASEKEY;
+const firebaseConfig = {
+  apiKey: "AIzaSyDuus-sPfqmqdEbNbK6TCVAFnRbLuFsPhU",
+  authDomain: "photoalbum-36ef9.firebaseapp.com",
+  projectId: "photoalbum-36ef9",
+  storageBucket: "photoalbum-36ef9.appspot.com",
+  messagingSenderId: "851111725931",
+  appId: "1:851111725931:web:34dc005b82bca194041a2e",
+  measurementId: "G-Z7K3JHNCP1",
+};
 
 export const app = initializeApp(firebaseConfig);
 
